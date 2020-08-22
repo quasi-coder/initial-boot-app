5b81c6c9ea80bba4f006d6035718270cbb9bb3d4
Divyashree <divyashreedwivedi@icloud.com>
2020-08-22 19:08:42 +0530

    first commit

---
+++ src/main/java/com/dwiveddi/boot/InitialBootAppApplication.java
@@ -0,0 +1,26 @@
+package com.dwiveddi.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class InitialBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialBootAppApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping("/api")
+	public static class ApiController{
+		@GetMapping("/greeting")
+		public String getGreeting(){
+			return "Hello from the world of API";
+		}
+
+	}
+
+}
